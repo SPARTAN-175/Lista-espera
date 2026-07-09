1169d41bd6e76223c2e610de93b2fbbb98be374c
SPARTAN-175 <avengers_0077@hotmail.com>
2026-07-08 19:26:22 -0600

    Update consulta.js

--- js/consulta.js
+++ js/consulta.js
@@ -1,2 +1,22 @@
 
 console.log("Consulta.js funcionando");
+
+/*
+=========================================
+Proyecto : Lista de Espera
+Archivo   : consulta.js
+Versión   : v2.0.0
+Autor     : Carlos & ChatGPT
+=========================================
+*/
+
+import { escucharLista } from "./firebase.js";
+
+console.log("Conectando con Firebase...");
+
+escucharLista((personas)=>{
+
+    console.log(personas);
+
+});
+
