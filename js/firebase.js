c4a11eecf5ca56b468222d88f316074dd3335fe9
SPARTAN-175 <avengers_0077@hotmail.com>
2026-07-27 15:15:19 -0600

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,50 +1,35 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
 import {
-    getFirestore,
-    collection,
-    query,
-    orderBy,
-    onSnapshot
+    getFirestore
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+/*==================================
+CONFIGURACIÓN DE FIREBASE
+==================================*/
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBnhuampwb_9Jhw7Ki2CYcwt2PZIgiqmE8",
-  authDomain: "lista-de-espera-6b89d.firebaseapp.com",
-  projectId: "lista-de-espera-6b89d",
-  storageBucket: "lista-de-espera-6b89d.firebasestorage.app",
-  messagingSenderId: "19656055004",
-  appId: "1:19656055004:web:a3a4ab6eb7d45de3e7fc19",
-  measurementId: "G-30V2P6CZ22"
+
+    apiKey: "AIzaSyBnhuampwb_...",
+
+    authDomain: "lista-de-espera-6b89d.firebaseapp.com",
+
+    projectId: "lista-de-espera-6b89d",
+
+    storageBucket: "lista-de-espera-6b89d.firebasestorage.app",
+
+    messagingSenderId: "19656055004",
+
+    appId: "1:19656055004:web:a3a4ab6eb7d45de3e7fc19"
+
 };
+
+/*==================================
+INICIALIZAR FIREBASE
+==================================*/
 
 const app = initializeApp(firebaseConfig);
 
-console.log("✅ Firebase iniciado");
-
 const db = getFirestore(app);
 
-const listaRef = collection(db, "lista");
-
-const listaQuery = query(listaRef, orderBy("turno", "asc"));
-
-export function escucharLista(callback){
-
-    onSnapshot(listaQuery,(snapshot)=>{
-
-        const personas=[];
-
-        snapshot.forEach(doc=>{
-
-            personas.push({
-                id:doc.id,
-                ...doc.data()
-            });
-
-        });
-
-        callback(personas);
-
-    });
-
-}
+export { db };
