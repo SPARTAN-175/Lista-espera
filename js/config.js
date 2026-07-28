4b0d186e8d7d773b15da458030237ec5e3eaf2bf
SPARTAN-175 <avengers_0077@hotmail.com>
2026-07-27 19:24:58 -0600

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,17 @@
+// ==================================
+// CONFIGURACIÓN GENERAL DEL SISTEMA
+// ==================================
+
+export const APP = {
+
+    nombre: "Sistema de Lista de Espera",
+
+    version: "1.0.0",
+
+    institucion: null,
+
+    usuario: null,
+
+    sesion: null
+
+};
