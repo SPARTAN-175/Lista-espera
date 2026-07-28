0a18abe073216af072f358777b897ea766b4b6f0
SPARTAN-175 <avengers_0077@hotmail.com>
2026-07-27 21:19:46 -0600

    Update registro.js

--- js/registro.js
+++ js/registro.js
@@ -1,26 +1,72 @@
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
 
 import {
     createUserWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+import {
+    doc,
+    setDoc,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-export async function registrarUsuario(correo, password) {
 
-    try {
+export async function registrarUsuario(datos){
 
-        const credencial = await createUserWithEmailAndPassword(
-            auth,
-            correo,
-            password
-        );
+    const credencial = await createUserWithEmailAndPassword(
+        auth,
+        datos.correo,
+        datos.password
+    );
 
-        return credencial.user;
+    const uid = credencial.user.uid;
 
-    } catch (error) {
+    const institucionId = crypto.randomUUID();
 
-        throw error;
+    await setDoc(
+        doc(db,"instituciones",institucionId),
+        {
 
-    }
+            nombre: datos.institucion,
+            fechaRegistro: serverTimestamp(),
+            activa: true
+
+        }
+    );
+
+    await setDoc(
+        doc(
+            db,
+            "instituciones",
+            institucionId,
+            "usuarios",
+            uid
+        ),
+        {
+
+            uid,
+
+            nombre: datos.administrador,
+
+            correo: datos.correo,
+
+            telefono: datos.telefono,
+
+            rol: "administrador",
+
+            activo: true,
+
+            fechaRegistro: serverTimestamp()
+
+        }
+    );
+
+    return {
+
+        uid,
+
+        institucionId
+
+    };
 
 }
